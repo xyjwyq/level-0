97301c19c544ac535be4d62c112c12bd06455147
xyjwyq <457238777@qq.com>
2019-10-07 22:30:05 +0800

    函数防抖与函数节流

--- plugin/helper.js
+++ plugin/helper.js
@@ -13,18 +13,18 @@ window.myPlugin.inherit = (function () {
 
 // 对象混入
 window.myPlugin.mixin = function (obj1, obj2) {
-    // return Object.assign({}, obj1, obj2);
+    return Object.assign({}, obj1, obj2);
 
-    var res = {};
-    for (var prop in obj2) {
-        res[prop] = obj2[prop]
-    }
-    for (var prop in obj1) {
-        if (!(prop in res)) {
-            res[prop] = obj1[prop];
-        }
-    }
-    return res;
+    // var res = {};
+    // for (var prop in obj2) {
+    //     res[prop] = obj2[prop]
+    // }
+    // for (var prop in obj1) {
+    //     if (!(prop in res)) {
+    //         res[prop] = obj1[prop];
+    //     }
+    // }
+    // return res;
 }
 
 // 对象克隆
@@ -52,4 +52,79 @@ window.myPlugin.clone = function (target, deep) {
     } else {
         return target;
     }
+}
+
+// 函数防抖 —— 函数在动作结束后一段时间运行
+window.myPlugin.debounce = function (callback, time) {
+    var timer = null;
+    return function () {
+        clearTimeout(timer);
+        var args = arguments;
+        timer = setTimeout(function () {
+            callback.apply(null, args);
+        }, time);
+    }
+}
+
+// 函数节流 ——— 一段时间内只运行一次
+window.myPlugin.throttle = function (callback, duration, immediately) {
+    // 方式1
+    // var timer = null;
+    // return function() {
+    //     if (timer) {
+    //         return;
+    //     }
+    //     var args = arguments;
+    //     timer = setTimeout(function() {
+    //         callback.apply(null, args);
+    //         timer = null;
+    //     }, duration);
+    // }
+
+    // 方式2
+    // var time;
+    // return function() {
+    //     if (!time || (Date.now() - time) >= duration) {
+    //         callback.apply(null, arguments);
+    //         time = Date.now();
+    //     }
+    // }
+
+    // 方式3
+    // var time = 0;
+    // return function () {
+    //     var curTime = Date.now();
+    //     if (curTime - time >= duration) {
+    //         callback.apply(null, arguments);
+    //         time = curTime;
+    //     }
+    // }
+
+    // 方式4 
+    if (immediately === undefined) {
+        immediately = true;
+    }
+    if (immediately) {
+        var time;
+        return function () {
+            if (!time || (Date.now() - time) >= duration) {
+                callback.apply(null, arguments);
+                time = Date.now();
+            }
+        }
+    } else {
+        var timer = null;
+        return function () {
+            if (timer) {
+                return;
+            }
+            var args = arguments;
+            timer = setTimeout(function () {
+                callback.apply(null, args);
+                timer = null;
+            }, duration);
+        }
+
+    }
+
 }
